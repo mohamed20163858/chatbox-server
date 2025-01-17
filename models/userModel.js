812d230e24b89c0845576271bc373352616c75f9
Mohamed Saleh <mohamed20163858@gmail.com>
2025-01-17 22:47:49 +0200

    Add field name to signup

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,10 @@ const validator = require("validator");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
@@ -17,9 +21,9 @@ const userSchema = new Schema({
 });
 
 // static signup method
-userSchema.statics.signup = async function (email, password) {
+userSchema.statics.signup = async function (name, email, password) {
   // validation
-  if (!email || !password) {
+  if (!email || !password || !name) {
     throw Error("All fields must be filled");
   }
   if (!validator.isEmail(email)) {
@@ -38,7 +42,7 @@ userSchema.statics.signup = async function (email, password) {
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
 
-  const user = await this.create({ email, password: hash });
+  const user = await this.create({ name, email, password: hash });
 
   return user;
 };
