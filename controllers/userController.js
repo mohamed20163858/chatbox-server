18ffce8bb24221efc6ee744eadd6313dd7655712
Mohamed Saleh <mohamed20163858@gmail.com>
2025-01-17 23:31:04 +0200

    return name to successfully loggedin and signedup user

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,11 +11,12 @@ const loginUser = async (req, res) => {
 
   try {
     const user = await User.login(email, password);
+    const name = user.name;
 
     // create a token
     const token = createToken(user._id);
 
-    res.status(200).json({ email, token });
+    res.status(200).json({ name, email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -31,7 +32,7 @@ const signupUser = async (req, res) => {
     // create a token
     const token = createToken(user._id);
 
-    res.status(200).json({ email, token });
+    res.status(200).json({ name, email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
