623b9dd167b9f33f88bbbbf911a3208c62daaa0c
Mohamed Saleh <mohamed20163858@gmail.com>
2025-01-18 16:00:32 +0200

    Add security to check endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,14 +39,28 @@ const signupUser = async (req, res) => {
 };
 
 const checkUser = async (req, res) => {
-  const { email } = req.body;
-  const user = await User.findOne({ email }).select("-_id -__v");
-  if (user && user.name && user.password) {
-    const data = { ...user._doc };
-    delete data.password;
-    return res.json({ hasCredentials: true, ...data });
+  const token = req.headers["authorization"]?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
   }
-  return res.json({ hasCredentials: false });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // console.log(decoded);
+    const { app } = decoded;
+    if (app !== "Next.js") {
+      return res.status(403).json({ message: "Forbidden: Invalid token" });
+    }
+    const { email } = req.body;
+    const user = await User.findOne({ email }).select("-_id -__v");
+    if (user && user.name && user.password) {
+      const data = { ...user._doc };
+      delete data.password;
+      return res.json({ hasCredentials: true, ...data });
+    }
+    return res.json({ hasCredentials: false });
+  } catch (err) {
+    return res.status(403).json({ message: "Forbidden: Invalid token" });
+  }
 };
 
 module.exports = { signupUser, loginUser, checkUser };
